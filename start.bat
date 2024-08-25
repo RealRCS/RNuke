b468c6667b5bae2d5764170119c47c14c899cc6c
RCS <belokanofficial@gmail.com>
2024-08-26 00:23:06 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@ title RNuke
 setlocal
 
 :: Check if Python executable exists
-python --version 2>&1 | findstr " 3.11" >nul
+python --version 2>&1 | findstr " 3.12" >nul
 if %errorlevel% == 0 (
-    echo python 3.11.x and up are not supported by RNuke. Please downgrade to python 3.10.x.
+    echo python 3.12.x and up are not supported by RNuke. Please downgrade to python 3.10.x.
     pause
     exit
 )
